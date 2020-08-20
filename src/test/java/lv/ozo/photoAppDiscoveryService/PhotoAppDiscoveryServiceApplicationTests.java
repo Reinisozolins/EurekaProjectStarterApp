62eba5efbca99ca51f8952671711dd3f03e6811c
Reinis Ozo <reinisozolins@mail.com>
2020-08-20 23:29:48 +0300

    Raw eureka project

---
+++ src/test/java/lv/ozo/photoAppDiscoveryService/PhotoAppDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.ozo.photoAppDiscoveryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
